97576b315b81e4b00c77505ac700a078fc5d7671
y00z <550823517@qq.com>
2017-07-21 11:46:36 +0800

    增加错误信息，优化webview

--- app/Home/index.js
+++ app/Home/index.js
@@ -1,23 +1,41 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View,WebView} from 'react-native';
+import {StyleSheet, Text, View, WebView} from 'react-native';
 
 
 class Home extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            map_live_url : 'http://123.57.39.116:3000/html/nearby.html',
+            map_live_url: 'http://123.57.39.116:3000/html/nearby.html',
+            isLoadError: false
         }
     }
 
 
     render() {
         return (
-            <WebView
-                //source={{uri: this.state.map_live_url}}
-                source={require('./../../resource/html/nearby.html')}
-            />
+            <View style={{flex : 1}}>
+                {
+                    this.state.isLoadError ?
+                        <Text style={styles.loadErrorText}>SORRY,加载出错.</Text>
+                        :
+                        <WebView
+                            //source={{uri: this.state.map_live_url}}
+                            source={require('./../../resource/html/nearby.html')}
+                            onError={()=>this._onError()}
+                            //加载中Loading
+                            startInLoadingState={true}
+                        />
+                }
+
+            </View>
         );
+    }
+
+    _onError = () => {
+        this.setState({
+            isLoadError: true
+        })
     }
 }
 
@@ -38,6 +56,13 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    loadErrorText: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 25
+    }
 });
 
 export default Home;
