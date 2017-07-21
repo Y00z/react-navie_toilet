c7f56f3e7bc6ea32e1bab644b6e1284521299f5e
y00z <550823517@qq.com>
2017-07-21 13:30:15 +0800

    完成天气页面Tab

--- app/Weather/index.js
+++ app/Weather/index.js
@@ -1,24 +1,40 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, WebView} from 'react-native';
 
 
 class Weather extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoadError: false
+        }
+    }
 
-    static navigationOptions = {
-        // title 可以这样设置成一个函数， state 会自动传过来
-        title: ({state}) => `${state.params.name}`,
-    };
+
     render() {
         return (
-            <View>
-                <Text>Weather</Text>
+            <View style={{flex : 1}}>
+                {
+                    this.state.isLoadError ?
+                        <Text style={styles.loadErrorText}>SORRY,加载出错.</Text>
+                        :
+                        <WebView
+                            //source={{uri: this.state.map_live_url}}
+                            source={require('./../../resource/html/weather.html')}
+                            onError={()=>this._onError()}
+                            //加载中Loading
+                            startInLoadingState={true}
+                        />
+                }
+
             </View>
         );
     }
 
-    componentDidMount() {
-        const {params} = this.props.navigation.state;
-        console.log(params)
+    _onError = () => {
+        this.setState({
+            isLoadError: true
+        })
     }
 }
 
@@ -39,6 +55,13 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    loadErrorText: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 25
+    }
 });
 
 export default Weather;
